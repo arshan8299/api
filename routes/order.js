a77e7a6ef68ecf6db80be2a3a082c36dc7c0d086
arshan8299 <97838233+arshan8299@users.noreply.github.com>
2024-02-19 23:41:43 +0530

    fin

--- routes/order.js
+++ routes/order.js
@@ -11,6 +11,7 @@ router.post("/",  async (req, res) => {
   try {
     const savedOrder = await newOrder.save();
     res.status(200).json(savedOrder);
+    
   } catch (err) {
     res.status(500).json(err);
   }
